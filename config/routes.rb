c8b00d653c7b8b0b8a08e0421ccfe9fc48fb0047
EC2 Default User <ec2-user@ip-172-31-36-252.ec2.internal>
2021-11-15 07:31:38 +0000

    5th Update

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   #patch 'books/:id' => 'books#update', as: 'update_book'
   #delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
-
+  ###
 end
